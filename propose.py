df30da1fa6592c4223029c6bad5fbacd2974e339
Sunil0881 <112623005+Sunil0881@users.noreply.github.com>
2023-03-13 21:44:57 +0530

    Add files via upload

---
+++ propose.py
@@ -0,0 +1,35 @@
+import turtle
+
+# Create a Turtle object
+pen = turtle.Turtle()
+
+# Set the pen color and fill color
+pen.color('red', 'red')
+
+# Set the pen size and speed
+pen.pensize(3)
+pen.speed(1)
+
+# Draw the heart
+pen.begin_fill()
+pen.left(45)
+pen.forward(100)
+pen.circle(50, 180)
+pen.right(90)
+pen.circle(50, 180)
+pen.forward(100)
+pen.end_fill()
+
+# Move to a new position for the text
+pen.penup()
+pen.goto(-90, -100)
+pen.pendown()
+
+# Write "I love you" using the font and size of your choice
+pen.write("I love you", font=("arial", 30, "normal"))
+
+# Hide the turtle when done
+pen.hideturtle()
+
+# Keep the window open until manually closed
+turtle.done()
